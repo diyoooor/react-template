92029d1202555476c79285cffe502a708eeaba53
Chutiphong <chutiphong.charoenchao@gmail.com>
2022-07-07 18:50:49 +0700

    learn: class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import EventComponent from "./event/EventComponent";
-
 function App() {
+  // const users = [
+  //   { name: "Max", age: 26 },
+  //   { name: "Xam", age: 19 },
+  //   { name: "Diyoor", age: 30 },
+  // ];
   return (
     <div className="App">
       {/* <GustList /> */}
       {/* <UserSearch /> */}
-      <EventComponent />
+      {/* <EventComponent /> */}
+      {/* <UserSearch users={users} /> */}
     </div>
   );
 }
