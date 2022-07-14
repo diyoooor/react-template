bfb6cf60723c89f87f49d7d7102afec57ebe1761
Chutiphong <chutiphong.charoenchao@gmail.com>
2022-07-14 15:48:56 +0700

    refactor: folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-import UserSearch from "./refs/UserSearch";
-
 function App() {
-  // const users = [
-  //   { name: "Max", age: 26 },
-  //   { name: "Xam", age: 19 },
-  //   { name: "Diyoor", age: 30 },
-  // ];
-  return (
-    <div className="App">
-      {/* <GustList /> */}
-      <UserSearch />
-      {/* <EventComponent /> */}
-      {/* <UserSearch users={users} /> */}
-    </div>
-  );
+  return <div className="App">Heelo WOrld</div>;
 }
 
 export default App;
