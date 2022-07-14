2308eeabed227299819d726f8c127cd7fe947f26
Chutiphong <chutiphong.charoenchao@gmail.com>
2022-07-14 17:05:46 +0700

    build: install mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import { ThemeProvider } from "@mui/system";
+import { AppBar } from "./components/AppBar";
+import theme from "./theme";
 function App() {
-  return <div className="App">Heelo WOrld</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <AppBar />
+      <div className="App">
+        <Button variant="outlined" color="secondary">
+          Heelo
+        </Button>
+        <Typography color="primary">Hello</Typography>
+        <Typography variant="h1">Hello</Typography>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  typography: {
+    fontSize: 12,
+    h1: {
+      color: "red",
+    },
+  },
+});
+
+export default theme;
